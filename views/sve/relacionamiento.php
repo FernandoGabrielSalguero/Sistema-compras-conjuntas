c57e1e0b13e6d861b3bb8c49f984c750b93f88c3
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2025-03-28 16:35:30 -0300

    main

--- views/sve/relacionamiento.php
+++ views/sve/relacionamiento.php
@@ -92,7 +92,7 @@ $relaciones = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alta productos</title>
+    <title>Relacionamiento</title>
     <style>
         .list-container {
             background-color: #F9FAFB;
