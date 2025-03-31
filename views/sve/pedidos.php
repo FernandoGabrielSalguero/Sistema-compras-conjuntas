b70d10f0b88d718abad2f9ea36bb8416da4cd6b5
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2025-03-31 09:57:03 -0300

    main

--- views/sve/pedidos.php
+++ views/sve/pedidos.php
@@ -188,12 +188,14 @@
     <!-- Sidebar -->
     <div id="sidebar">
         <nav>
-            <a href="sve_dashboard.php"><i class="fa fa-home"></i> Dashboard</a><br>
+        <a href="sve_dashboard.php"><i class="fa fa-home"></i> Inicio</a><br>
             <a href="alta_usuarios.php"><i class="fa fa-user-plus"></i> Alta Usuarios</a><br>
-            <a href="alta_fincas.php"><i class="fa fa-tree"></i> Alta Fincas</a><br>
+            <a href="relacionamiento.php"><i class="fa fa-user-plus"></i> Relacionamiento </a><br>
             <a href="alta_productos.php"><i class="fa fa-box"></i> Alta Productos</a><br>
             <a href="mercado_digital.php"><i class="fa fa-shopping-cart"></i> Mercado Digital</a><br>
             <a href="pedidos.php"><i class="fa fa-list"></i> Pedidos</a><br>
+            <a href="CargaMasivaUsuarios.php"><i class="fa fa-list"></i> Carga masiva de datos</a><br>
+            <a href="base_datos.php"><i class="fa fa-list"></i>  Base de datos </a><br>
             <a href="logout.php"><i class="fa fa-sign-out-alt"></i> Salir</a><br>
         </nav>
         <button id="close-menu-button" onclick="toggleSidebar()">Cerrar Menú</button>
