8caedd3decfa119f64bf27c50cb5546591fc00a6
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2025-04-04 14:46:58 -0300

    main

--- views/sve/mercado_digital.php
+++ views/sve/mercado_digital.php
@@ -554,17 +554,16 @@ if (isset($_POST['finalizar'])) {
 
         /* Asegura que flex funcione para centrar */
         #modalResumen {
-            display: none;
             position: fixed;
-            top: 0;
-            left: 0;
-            width: 100vw;
-            height: 100vh;
-            background-color: rgba(0, 0, 0, 0.5);
-            z-index: 9999;
-            display: flex;
-            justify-content: center;
-            align-items: center;
+    display: none; /* oculto por defecto */
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
         }
 
         .modal-botones button:first-child {
