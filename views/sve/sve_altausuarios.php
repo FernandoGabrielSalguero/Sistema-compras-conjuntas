8d88b1c388edb378f580986e9d5c0c5611febb75
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2025-04-09 11:43:31 -0300

    main

--- views/sve/sve_altausuarios.php
+++ views/sve/sve_altausuarios.php
@@ -90,152 +90,167 @@ $observaciones = $_SESSION['observaciones'] ?? 'Sin observaciones';
                     <form class="form-modern">
                         <div class="form-grid grid-4">
 
-
-                            <!-- CUIT -->
+                            <!-- CUIT ok -->
                             <div class="input-group">
-                                <label for="cuit">Cuit</label>
+                                <label for="cuit">CUIT
+                                    <span class="tooltip" data-tooltip="Ej: 20-12345678-1">
+                                        <span class="material-icons">info</span>
+                                    </span>
+                                </label>
                                 <div class="input-icon">
-                                    <span class="material-icons">location_city</span>
-                                    <input type="text" id="cuit" name="cuit" required>
+                                    <span class="material-icons">badge</span>
+                                    <input type="text" id="cuit" name="cuit" pattern="[0-9]{2}-[0-9]{8}-[0-9]{1}"
+                                        required>
                                 </div>
                             </div>
-
-                            <!-- Contraseña -->
-                            <div class="input-group">
-                                <label for="cp">Contraseña</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">markunread_mailbox</span>
-                                    <input type="text" id="cp" name="cp" pattern="[0-9]{4,6}" required>
-                                </div>
-                            </div>
-
-                            <!-- Rol -->
-                            <div class="input-group">
-                                <label for="rol">Rol</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">public</span>
-                                    <select id="rol" name="rol" required>
-                                        <option value="">Seleccionar</option>
-                                        <option value="sve">SVE</option>
-                                        <option value="cooperativa">Cooperativa</option>
-                                        <option value="productor">Productor</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <!-- Permiso -->
-                            <div class="input-group">
-                                <label for="permiso">Permiso</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">public</span>
-                                    <select id="permiso" name="permiso" required>
-                                        <option value="Habilitado">Habilitado</option>
-                                        <option value="Deshabilitado">No habilitado</option>
-
-                                    </select>
-                                </div>
-                            </div>
-
-                             <!-- CUIT -->
-                             <div class="input-group">
-                                <label for="cuit">Cuit</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">location_city</span>
-                                    <input type="text" id="cuit" name="cuit" required>
-                                </div>
-                            </div>
-
-                            <!-- Contraseña -->
-                            <div class="input-group">
-                                <label for="cp">Contraseña</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">markunread_mailbox</span>
-                                    <input type="text" id="cp" name="cp" pattern="[0-9]{4,6}" required>
-                                </div>
-                            </div>
-
-                            <!-- Rol -->
-                            <div class="input-group">
-                                <label for="rol">Rol</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">public</span>
-                                    <select id="rol" name="rol" required>
-                                        <option value="">Seleccionar</option>
-                                        <option value="sve">SVE</option>
-                                        <option value="cooperativa">Cooperativa</option>
-                                        <option value="productor">Productor</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <!-- Permiso -->
-                            <div class="input-group">
-                                <label for="permiso">Permiso</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">public</span>
-                                    <select id="permiso" name="permiso" required>
-                                        <option value="Habilitado">Habilitado</option>
-                                        <option value="Deshabilitado">No habilitado</option>
-
-                                    </select>
-                                </div>
-                            </div>
-
-                             <!-- CUIT -->
-                             <div class="input-group">
-                                <label for="cuit">Cuit</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">location_city</span>
-                                    <input type="text" id="cuit" name="cuit" required>
-                                </div>
-                            </div>
-
-                            <!-- Contraseña -->
-                            <div class="input-group">
-                                <label for="cp">Contraseña</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">markunread_mailbox</span>
-                                    <input type="text" id="cp" name="cp" pattern="[0-9]{4,6}" required>
-                                </div>
-                            </div>
-
-                            <!-- Rol -->
-                            <div class="input-group">
-                                <label for="rol">Rol</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">public</span>
-                                    <select id="rol" name="rol" required>
-                                        <option value="">Seleccionar</option>
-                                        <option value="sve">SVE</option>
-                                        <option value="cooperativa">Cooperativa</option>
-                                        <option value="productor">Productor</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <!-- Permiso -->
-                            <div class="input-group">
-                                <label for="permiso">Permiso</label>
-                                <div class="input-icon">
-                                    <span class="material-icons">public</span>
-                                    <select id="permiso" name="permiso" required>
-                                        <option value="Habilitado">Habilitado</option>
-                                        <option value="Deshabilitado">No habilitado</option>
-
-                                    </select>
-                                </div>
-                            </div>
-
-
-
-
-
-
-                        <!-- Botones -->
-                        <div class="form-buttons">
-                            <button class="btn btn-aceptar" type="submit">Enviar</button>
-                            <button class="btn btn-cancelar" type="reset">Cancelar</button>
                         </div>
+
+                            <!-- Contraseña -->
+                            <div class="input-group">
+                                <label for="cp">Contraseña</label>
+                                <div class="input-icon">
+                                    <span class="material-icons">markunread_mailbox</span>
+                                    <input type="text" id="cp" name="cp" pattern="[0-9]{4,6}" required>
+                                </div>
+                            </div>
+
+                            <!-- Rol -->
+                            <div class="input-group">
+                                <label for="rol">Rol</label>
+                                <div class="input-icon">
+                                    <span class="material-icons">public</span>
+                                    <select id="rol" name="rol" required>
+                                        <option value="">Seleccionar</option>
+                                        <option value="sve">SVE</option>
+                                        <option value="cooperativa">Cooperativa</option>
+                                        <option value="productor">Productor</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <!-- Permiso -->
+                            <div class="input-group">
+                                <label for="permiso">Permiso</label>
+                                <div class="input-icon">
+                                    <span class="material-icons">public</span>
+                                    <select id="permiso" name="permiso" required>
+                                        <option value="Habilitado">Habilitado</option>
+                                        <option value="Deshabilitado">No habilitado</option>
+
+                                    </select>
+                                </div>
+                            </div>
+
+                            <!-- nombre ok-->
+                            <div class="input-group">
+                                <label for="nombre">Nombre completo
+                                    <span class="tooltip" data-tooltip="Ingresá tu nombre y apellido completo.">
+                                        <span class="material-icons">info</span>
+                                    </span>
+                                </label>
+                                <div class="input-icon">
+                                    <span class="material-icons">person</span>
+                                    <input type="text" id="nombre" name="nombre" placeholder="Juan Pérez" required
+                                        minlength="2" maxlength="60" aria-required="true">
+                                </div>
+                                <small class="error-message" aria-live="polite"></small>
+                            </div>
+
+                            <!-- correo OK-->
+                            <div class="input-group">
+                                <label for="email">Correo electrónico
+                                    <span class="tooltip" data-tooltip="Ej: usuario@correo.com">
+                                        <span class="material-icons">info</span>
+                                    </span>
+                                </label>
+                                <div class="input-icon">
+                                    <span class="material-icons">mail</span>
+                                    <input type="email" id="email" name="email" placeholder="usuario@correo.com"
+                                        required aria-required="true">
+                                </div>
+                                <small class="error-message" aria-live="polite"></small>
+                            </div>
+                        </div>
+
+                            <!-- telefono ok -->
+                            <div class="input-group">
+                                <label for="telefono">Teléfono
+                                    <span class="tooltip" data-tooltip="Incluí el código de área. Ej: +54 11 5555-1234">
+                                        <span class="material-icons">info</span>
+                                    </span>
+                                </label>
+                                <div class="input-icon">
+                                    <span class="material-icons">phone</span>
+                                    <input type="tel" id="telefono" name="telefono" pattern="[0-9\+\-\s]{7,20}"
+                                        required>
+                                </div>
+                            </div>
+
+                            <!-- id_cooperativa -->
+                            <div class="input-group">
+                                <label for="permiso">Permiso</label>
+                                <div class="input-icon">
+                                    <span class="material-icons">public</span>
+                                    <select id="permiso" name="permiso" required>
+                                        <option value="Habilitado">Habilitado</option>
+                                        <option value="Deshabilitado">No habilitado</option>
+
+                                    </select>
+                                </div>
+                            </div>
+
+                            <!-- id_productor -->
+                            <div class="input-group">
+                                <label for="cuit">Cuit</label>
+                                <div class="input-icon">
+                                    <span class="material-icons">location_city</span>
+                                    <input type="text" id="cuit" name="cuit" required>
+                                </div>
+                            </div>
+
+                            <!-- direccion OK-->
+                            <div class="input-group">
+                                <label for="direccion">Dirección</label>
+                                <div class="input-icon">
+                                    <span class="material-icons">home</span>
+                                    <input type="text" id="direccion" name="direccion" required>
+                                </div>
+                            </div>
+                        </div>
+
+                            <!-- id_finca_asociada -->
+                            <div class="input-group">
+                                <label for="rol">Rol</label>
+                                <div class="input-icon">
+                                    <span class="material-icons">public</span>
+                                    <select id="rol" name="rol" required>
+                                        <option value="">Seleccionar</option>
+                                        <option value="sve">SVE</option>
+                                        <option value="cooperativa">Cooperativa</option>
+                                        <option value="productor">Productor</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <!-- observaciones -->
+                            <div class="input-group">
+                                <label for="permiso">Permiso</label>
+                                <div class="input-icon">
+                                    <span class="material-icons">public</span>
+                                    <select id="permiso" name="permiso" required>
+                                        <option value="Habilitado">Habilitado</option>
+                                        <option value="Deshabilitado">No habilitado</option>
+
+                                    </select>
+                                </div>
+                            </div>
+
+
+                            <!-- Botones -->
+                            <div class="form-buttons">
+                                <button class="btn btn-aceptar" type="submit">Enviar</button>
+                                <button class="btn btn-cancelar" type="reset">Cancelar</button>
+                            </div>
                     </form>
                 </div>
 
