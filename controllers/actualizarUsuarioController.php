84ab1c90366df8fdf97918042fe433ea32a82cf2
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2025-04-10 13:31:43 -0300

    main

--- controllers/actualizarUsuarioController.php
+++ controllers/actualizarUsuarioController.php
@@ -1,4 +1,9 @@
 <?php
+// Mostrar errores en pantalla (útil en desarrollo)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../config.php';
 
 $id = $_POST['id'] ?? null;
--- controllers/altaOperativoController.php
+++ controllers/altaOperativoController.php
@@ -1,4 +1,9 @@
 <?php
+// Mostrar errores en pantalla (útil en desarrollo)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
 require_once __DIR__ . '/../models/OperativosModel.php';
 require_once __DIR__ . '/../config/database.php';
