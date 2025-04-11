d8a432f1b358cc7e5645b8bf9a22398c741bcc0f
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2025-04-11 16:03:34 -0300

    main

--- views/sve/sve_mercadodigital.php
+++ views/sve/sve_mercadodigital.php
@@ -221,7 +221,7 @@ $observaciones = $_SESSION['observaciones'] ?? 'Sin observaciones';
                     </div>
 
                     <!-- Acordeones de productos (dinámicos desde JS) -->
-                    <div class="">
+                    <div class="form-grid grid-2">
                         <div id="acordeones-productos"></div>
                     </div>
 
