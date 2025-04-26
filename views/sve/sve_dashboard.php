7c39695c931b9ff2f69082db50ba5b0d3a1ec7db
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-04-26 09:55:35 -0300

    main

--- views/sve/sve_dashboard.php
+++ views/sve/sve_dashboard.php
@@ -62,10 +62,10 @@ $observaciones = $_SESSION['observaciones'] ?? 'Sin observaciones';
                     <li onclick="location.href='sve_operativos.php'">
                         <span class="material-icons">assignment</span><span class="link-text">Operativos</span>
                     </li>
-                    <li onclick="location.href='sve_productos.php'">
+                    <li onclick="location.href='sve_mercadodigital.php'">
                         <span class="material-icons">shopping_cart</span><span class="link-text">Mercado Digital</span>
                     </li>
-                    <li onclick="location.href='sve_mercadodigital.php'">
+                    <li onclick="location.href='sve_productos.php'">
                         <span class="material-icons">inventory</span><span class="link-text">Productos</span>
                     </li>
                 </ul>
