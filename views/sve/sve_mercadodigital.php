13f4cb7ea2ce69a111ea86fb482afc070d7198a8
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2025-05-10 12:32:02 -0300

    main

--- views/sve/sve_mercadodigital.php
+++ views/sve/sve_mercadodigital.php
@@ -96,6 +96,7 @@ $observaciones = $_SESSION['observaciones'] ?? 'Sin observaciones';
         }
 
         #productosEditablesContainer .card {
+            background-color: #f3f0ff;
             padding: 1rem;
             border-radius: 10px;
             box-shadow: 0 2px 6px rgba(0, 0, 0, 0.08);
