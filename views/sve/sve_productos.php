350231ff13e73fcc6a73c1de50ba819dd38fc4a9
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-05-11 16:56:08 -0300

    main

--- views/sve/sve_productos.php
+++ views/sve/sve_productos.php
@@ -266,7 +266,8 @@ $observaciones = $_SESSION['observaciones'] ?? 'Sin observaciones';
         </div>
     </div>
 
-    <script src="/assets/js/sve_productos.js"></script>
+    <script src="/assets/js/sve_productos.js?v=<?= time() ?>"></script>
+
 
     <!-- Script para cargar los datos usando AJAX a la base -->
     <script>
