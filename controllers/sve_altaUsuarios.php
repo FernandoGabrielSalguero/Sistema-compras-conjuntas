7fb55ea7cd7dcf73892f37e0161a6248402b2317
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-05-17 09:44:03 -0300

    main

--- controllers/sve_altaUsuarios.php
+++ controllers/sve_altaUsuarios.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 session_start();
 
-require_once __DIR__ . '/../models/sve_altausuarios.php';
+require_once __DIR__ . '/../models/sve_altaUsuarios.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userModel = new UserModel($pdo);
--- views/sve/sve_altausuarios.php
+++ views/sve/sve_altausuarios.php
@@ -325,7 +325,7 @@ $telefono = $_SESSION['telefono'] ?? 'Sin teléfono';
                     if (result.success) {
                         form.reset();
                         showAlert('success', result.message); // ✅ alerta verde
-                        cargarUsuarios(); // 👈 actualiza la tabla
+                        // cargarUsuarios(); // 👈 actualiza la tabla
                     } else {
                         showAlert('error', result.message); // ❌ alerta roja
                     }
