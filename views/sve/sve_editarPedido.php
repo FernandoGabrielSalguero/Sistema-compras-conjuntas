359ad8764f64b35853fea0e35efcc35c0fbdb223
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-05-23 18:53:00 -0300

    main

--- views/sve/sve_editarPedido.php
+++ views/sve/sve_editarPedido.php
@@ -1,4 +1,9 @@
 <?php
+// Mostrar errores en pantalla (útil en desarrollo)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../models/sve_MercadoDigitalModel.php';
