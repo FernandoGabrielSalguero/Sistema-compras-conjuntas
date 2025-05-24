a7d09ead409fb692dd3f1891d06729c917fc6ef9
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-05-23 23:12:45 -0300

    main

--- views/sve/sve_editarPedido.php
+++ views/sve/sve_editarPedido.php
@@ -5,8 +5,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-require_once $_SERVER['DOCUMENT_ROOT'] . '/compra_conjunta/config.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/compra_conjunta/models/sve_MercadoDigitalModel.php';
+require_once __DIR__ . '/../../config.php';
+require_once __DIR__ . '/../../models/sve_MercadoDigitalModel.php';
 
 if (!isset($_SESSION['usuario']) || $_SESSION['rol'] !== 'sve') {
     die("Acceso denegado");
