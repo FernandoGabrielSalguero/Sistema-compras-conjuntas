e0409d18d6bf3acf1db15733829e3604425920ba
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2025-05-28 11:53:46 -0300

    main

--- views/cooperativa/coop_dashboard.php
+++ views/cooperativa/coop_dashboard.php
@@ -75,10 +75,7 @@ $telefono = $_SESSION['telefono'] ?? 'Sin teléfono';
                         <span class="material-icons" style="color: #5b21b6;">shopping_cart</span><span class="link-text">Mercado Digital</span>
                     </li>
                     <li onclick="location.href='coop_listadoPedidos.php'">
-                        <span class="material-icons" style="color: #5b21b6;">assignment_turned_in</span><span class="link-text">Listado Pedidos</span>
-                    </li>
-                    <li onclick="location.href='coop_pedidos.php'">
-                        <span class="material-icons" style="color: #5b21b6;">receipt_long</span><span class="link-text">Pedidos</span>
+                        <span class="material-icons" style="color: #5b21b6;">receipt_long</span><span class="link-text">Listado Pedidos</span>
                     </li>
                     <li onclick="location.href='coop_productores.php'">
                         <span class="material-icons" style="color: #5b21b6;">groups</span><span class="link-text">Productores</span>
