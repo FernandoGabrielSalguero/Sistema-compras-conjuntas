ff44b900e43a9bf5483aa02cbc9dc7885a507c51
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-07-16 09:58:13 -0300

    main

--- views/cooperativa/coop_listadoPedidos.php
+++ views/cooperativa/coop_listadoPedidos.php
@@ -280,8 +280,8 @@ echo "<script>console.log('🟣 id_cooperativa desde PHP: " . $id_cooperativa_re
                     </td>
                     <td>${p.nombre_operativo || '-'}</td>
                     <td>
-                        <button class="btn-icon" onclick="verPedido(${p.id})" tooltip="Ver pedido completo">
-                            <i class="material-icons" style="color:blue;">description</i>
+                        <button class="btn-icon" onclick="verPedido(${p.id})">
+                            <i class="material-icons" style="color:blue; data-tooltip="Subir factura"">description</i>
                         </button>
                         ${p.estado_operativo === 'abierto' 
                             ? `<button class="btn-icon" onclick="abrirModalEdicion(${p.id})">
