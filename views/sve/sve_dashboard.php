669eabff7253c7a984964bf50a5d8d3f05413d4f
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-08-28 11:45:08 -0300

    main

--- views/sve/sve_dashboard.php
+++ views/sve/sve_dashboard.php
@@ -77,6 +77,9 @@ unset($_SESSION['cierre_info']); // Limpiamos para evitar residuos
                     <li onclick="location.href='sve_productos.php'">
                         <span class="material-icons" style="color: #5b21b6;">inventory</span><span class="link-text">Productos</span>
                     </li>
+                    <li onclick="location.href='sve_pulverizacionDrone.php'">
+                        <span class="material-icons" style="color: #5b21b6;">flight</span><span class="link-text">Drones</span>
+                    </li>
                     <li onclick="location.href='sve_publicaciones.php'">
                         <span class="material-icons" style="color: #5b21b6;">menu_book</span><span class="link-text">Biblioteca Virtual</span>
                     </li>
