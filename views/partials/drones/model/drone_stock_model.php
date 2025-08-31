a31391dc7accf8a94bfde92d11f67a31bf1b0c45
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-08-31 12:44:19 -0300

    main

--- views/partials/drones/model/drone_stock_model.php
+++ views/partials/drones/model/drone_stock_model.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * Modelo mínimo SIN métodos: sólo metadatos y referencia a PDO.
  * El controlador le inyecta $pdo (desde config.php).
  */
-class DroneCalendarModel
+class DroneStockModel
 {
     /** @var PDO */
     public $pdo;
