c00a726780a1aaf4c5812c136075d7a43cf0c6b6
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-10-08 02:07:07 -0300

    main

--- views/drone_pilot/drone_pilot_dashboard.php
+++ views/drone_pilot/drone_pilot_dashboard.php
@@ -329,7 +329,7 @@ $sesionDebug = [
                 </div>
 
                 <!-- Mis solicitudes (tabla estándar) -->
-                <div class="card tabla-card" id="card-solicitudes" style="background-color: #d6e6e6ff;">
+                <div class="card tabla-card" id="card-solicitudes" style="background-color: #f3f0ff;">
                     <div class="flex items-center justify-between">
                         <h2>Mis solicitudes asignadas</h2>
                     </div>
