0db96714beca1d980ceacdad378c791c530d71f5
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-11-02 15:51:36 -0300

    main

--- views/ingeniero/ing_dashboard.php
+++ views/ingeniero/ing_dashboard.php
@@ -97,16 +97,18 @@ unset($_SESSION['cierre_info']);
                             <span class="link-text">Pulverización</span>
                         </a>
                     </li>
+
+                    <li>
+                        <a href="ing_servicios.php">
+                            <span class="material-icons">chevron_right</span>
+                            <span class="link-text">Pulverización</span>
+                        </a>
+                    </li>
                     <!-- Agregá más ítems aquí cuando existan nuevas hojas de Drones -->
                 </ul>
 
-
                 <!-- Resto de opciones -->
                 <ul>
-                    <li onclick="location.href='ing_servicios.php'">
-                        <span class="material-icons" style="color:#5b21b6;">upload_file</span>
-                        <span class="link-text">Servicios</span>
-                    </li>
                     <li onclick="location.href='../../../logout.php'">
                         <span class="material-icons" style="color:red;">logout</span>
                         <span class="link-text">Salir</span>
