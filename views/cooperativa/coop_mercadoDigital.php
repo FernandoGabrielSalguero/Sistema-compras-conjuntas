c8014b4f98a95db849f8b2ed8e8eacd09e89126e
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-12-10 09:03:40 -0300

    main

--- views/cooperativa/coop_mercadoDigital.php
+++ views/cooperativa/coop_mercadoDigital.php
@@ -235,30 +235,37 @@ echo "<script>console.log('🟣 id_cooperativa desde PHP: " . $id_cooperativa_re
             </div>
 
             <nav class="sidebar-menu">
-                <ul>
+<ul>
                     <li onclick="location.href='coop_dashboard.php'">
                         <span class="material-icons" style="color: #5b21b6;">home</span><span class="link-text">Inicio</span>
                     </li>
                     <li onclick="location.href='coop_mercadoDigital.php'">
-                        <span class="material-icons" style="color: #5b21b6;">shopping_cart</span><span class="link-text">Mercado Digital</span>
+                        <span class="material-icons"
+                            style="color: #5b21b6;">shopping_cart</span><span class="link-text">Mercado Digital</span>
                     </li>
                     <li onclick="location.href='coop_listadoPedidos.php'">
-                        <span class="material-icons" style="color: #5b21b6;">receipt_long</span><span class="link-text">Listado Pedidos</span>
+                        <span class="material-icons"
+                            style="color: #5b21b6;">receipt_long</span><span class="link-text">Listado Pedidos</span>
                     </li>
                     <li onclick="location.href='coop_consolidado.php'">
-                        <span class="material-icons" style="color: #5b21b6;">analytics</span><span class="link-text">Consolidado</span>
+                        <span class="material-icons"
+                            style="color: #5b21b6;">analytics</span><span class="link-text">Consolidado</span>
                     </li>
                     <li onclick="location.href='coop_pulverizacion.php'">
-                    <span class="material-symbols-outlined" style="color:#5b21b6;">drone</span><span class="link-text">Pulverización con Drone</span>
+                        <span class="material-symbols-outlined"
+                            style="color:#5b21b6;">drone</span><span class="link-text">Pulverización con Drone</span>
                     </li>
                     <li onclick="location.href='coop_usuarioInformacion.php'">
-                        <ure class="material-icons" style="color: #5b21b6;">person</ure><span class="link-text">Productores</span>
+                        <span class="material-icons" style="color: #5b21b6;">person</span><span
+                            class="link-text">Productores</span>
                     </li>
-                    <!-- <li onclick="location.href='coop_cosechaMecanicaView.php'">
-                        <ure class="material-icons" style="color: #5b21b6;">agriculture</ure><span class="link-text">Cosecha Mecanica</span>
-                    </li> -->
+                    <li onclick="location.href='coop_cosechaMecanicaView.php'">
+                        <span class="material-icons"
+                            style="color: #5b21b6;">agriculture</span><span class="link-text">Cosecha Mecanica</span>
+                    </li>
                     <li onclick="location.href='../../../logout.php'">
-                        <span class="material-icons" style="color: red;">logout</span><span class="link-text">Salir</span>
+                        <span class="material-icons" style="color: red;">logout</span><span
+                            class="link-text">Salir</span>
                     </li>
                 </ul>
             </nav>
