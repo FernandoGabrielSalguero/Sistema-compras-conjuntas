8e33f97ee9cb8523aa366deb728cce4603dff7a6
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-12-18 18:59:42 -0300

    main

--- views/partials/carga_masiva/cargaDatosFamiliaView.php
+++ views/partials/carga_masiva/cargaDatosFamiliaView.php
@@ -17,7 +17,7 @@
         <div style="display:flex;gap:8px;flex-wrap:wrap;">
             <button class="btn btn-acces" type="button" id="familiaSchemaBtn">Verificar esquema</button>
             <button class="btn btn-info" type="button" id="familiaSimBtn">Simular (rollback)</button>
-            <button class="btn btn-acces" type="button" id="familiaRunBtn">Cargar (commit)</button>
+            <button class="btn btn-info" type="button" id="familiaRunBtn">Cargar (commit)</button>
         </div>
 
     </div>
